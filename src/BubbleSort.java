51ba37f6047fa150cd7a440ea005c9dbb6b4f234
Zhu <964237881@qq.com>
2019-08-16 11:21:33 +0800

    冒泡排序

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,7 +1,9 @@
 public class BubbleSort {
     //时间复杂度O(n^2)
     public static void bubbleSort(int[] arr){
-        for (int i = 0; i < arr.length - 1; ++i){//理论上需要n-1次
+        if(arr == null || arr.length == 0)
+            return;
+        for (int i = 0; i <= arr.length - 2; ++i){//理论上需要n-1次
             boolean exchange = false;//设置一个交换标志，如果某次没有交换，则说明已经排序完毕
             for (int j = 0; j <= arr.length - 2 - i; ++j){//判断相邻两个元素大小，大的放后面！注意j初始位置是0，并不是i！
                 if(arr[j] > arr[j + 1]){
@@ -21,10 +23,12 @@ public class BubbleSort {
     }
 
     public static void main(String[] args) {
-        int [] test = {4,3,8,1,2,6,9};
+        //int [] test = {4,3,8,1,2,6,9};
+        //int [] test = {1,2,3,4,5};
+        int [] test = {5,4,3,2,1};
         bubbleSort(test);
         for (Integer integer: test){
-            System.out.print(integer);
+            System.out.print(integer + " ");
         }
         System.out.println();
     }
